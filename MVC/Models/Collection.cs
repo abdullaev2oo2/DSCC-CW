7752094cc9388a6c14b19604433ef899b45a4398
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-10-29 13:00:33 +0500

    feat: init WEB API

--- MVC/Models/Collection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVC.Models
-{
-    public class Collection
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    }
-}

0b7814b6a3ccb88283695efb64b64a0eac7b0537
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-11-05 14:44:48 +0500

    style: vs project solution and project paths

---
+++ Backend/Backend/Data/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Interfaces;
+using Backend.Repositories;
+using System.Threading.Tasks;
+
+namespace Backend.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext dc;
+        public UnitOfWork(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+        public IWriterRepository WriterRepository => new WriterRepository(dc);
+        public IBookRepository BookRepository => new BookRepository(dc);
+
+        public async Task<bool> SaveAsync()
+        {
+            return await dc.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Backend/Backend/Program.cs
@@ -0,0 +1,32 @@
+using Backend.Data;
+using Backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
